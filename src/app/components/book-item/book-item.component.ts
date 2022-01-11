0a4a4be684d683c8243ddcb1555e0207dc431dc2
Cwilder29 <cwilder101@gmail.com>
2022-01-11 17:04:47 -0600

    Added icons for boolean values for a book

--- src/app/components/book-item/book-item.component.ts
+++ src/app/components/book-item/book-item.component.ts
@@ -8,10 +8,33 @@ import { Book } from '../../Book';
 })
 export class BookItemComponent implements OnInit {
   @Input() book: Book;
+  ownershipIcon: string;
+  hasReadIcon: string;
+  nowReadingIcon: string;
 
   constructor() { }
 
   ngOnInit(): void {
+    if (this.book.ownership) {
+      this.ownershipIcon = '../../../assets/green-checkmark.png';
+    }
+    else {
+      this.ownershipIcon = '../../../assets/red-x.png';
+    }
+
+    if (this.book.hasRead) {
+      this.hasReadIcon = '../../../assets/green-checkmark.png'
+    }
+    else {
+      this.hasReadIcon = '../../../assets/red-x.png'
+    }
+
+    if (this.book.nowReading) {
+      this.nowReadingIcon = '../../../assets/green-checkmark.png'
+    }
+    else {
+      this.nowReadingIcon = '../../../assets/red-x.png'
+    }
   }
 
 }
